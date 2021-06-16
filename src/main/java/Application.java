4ba825e0743dccb1f84ea4ef561eb638813e3481
zhouxinqiang <409860217@qq.com>
2021-06-16 23:04:25 +0800

    push commit

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -6,6 +6,6 @@ public class Application {
         System.out.println("1234");
         System.out.println("456");
         System.out.println("789");
-
+        System.out.println("789");
     }
 }
